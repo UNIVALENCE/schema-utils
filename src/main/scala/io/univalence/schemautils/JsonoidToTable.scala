0f0a5b5a2c315e3e424a363f179b941c70eda752
Jonathan Winandy <jonathan.winandy@gmail.com>
2018-11-02 01:47:59 +0100

    minor changes

--- src/main/scala/io/univalence/schemautils/JsonoidToTable.scala
+++ src/main/scala/io/univalence/schemautils/JsonoidToTable.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql.{Column, DataFrame}
 
 case class AtomicFieldPath private (names: Vector[String] = Vector.empty, dataType: DataType) {
   def add(name: String, dataType: DataType): AtomicFieldPath = AtomicFieldPath(names :+ name, dataType)
-  def add(name: String): AtomicFieldPath                     = add(name, dataType)
+  def add(name: String): AtomicFieldPath = add(name, dataType)
 }
 
 object AtomicFieldPath {
@@ -19,28 +19,27 @@ object JsonoidToTable {
       field <- structType.fields
       fieldpath <- field.dataType match {
         case subStruct: StructType => allDirectlyAccessibleFields(subStruct, prefix.add(field.name))
-        case _: ArrayType          => Nil
-        case dt                    => Seq(prefix.add(field.name, dt))
+        case _:         ArrayType  => Nil
+        case dt => List(prefix.add(field.name, dt))
       }
     } yield fieldpath
   }
 
   def apply(df: DataFrame): DataFrame = {
+    val schema: StructType = df.schema
 
-    val schema = df.schema
+    //expr parses a sql expression like 'case a.b.c when 2 then 2 + 1 else d end as xyz' into a column
+    import org.apache.spark.sql.functions.expr
 
-    import org.apache.spark.sql.functions._
-
-    val cols: Seq[Column] = allDirectlyAccessibleFields(schema, AtomicFieldPath.empty) map { fieldPath =>
-      //val fieldref:Column = fieldPath.names.tail.foldLeft(df(fieldPath.names.head))(_ getField _)
-
+    val cols: Seq[Column] = for {
+      fieldPath <- allDirectlyAccessibleFields(schema, AtomicFieldPath.empty)
+    } yield {
       val fieldref: Column = expr(fieldPath.names.mkString("."))
-      val name: String     = fieldPath.names.mkString("_")
+      val name:     String = fieldPath.names.mkString("_")
 
       fieldref.as(name)
     }
 
     df.select(cols: _*)
-
   }
 }
