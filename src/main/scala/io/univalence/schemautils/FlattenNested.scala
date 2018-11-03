e59d8738443ac685e0e8e95261e329ae6c9353eb
Jonathan Winandy <jonathan.winandy@gmail.com>
2018-11-03 22:38:28 +0100

    start flatmap nested

--- src/main/scala/io/univalence/schemautils/FlattenNested.scala
+++ src/main/scala/io/univalence/schemautils/FlattenNested.scala
@@ -2,10 +2,11 @@ package io.univalence.schemautils
 import org.apache.spark.sql.DataFrame
 
 object FlattenNested {
+
   def apply(dataFrame: DataFrame):DataFrame = {
 
 
-
+    import org.apache.spark.sql.functions._
 
     ???
   }
---
+++ src/test/scala/io/univalence/schemautils/FlattenNestedTest.scala
@@ -0,0 +1,9 @@
+package io.univalence.schemautils
+import org.scalatest.FunSuite
+
+class FlattenNestedTest extends FunSuite {
+
+
+  test("testApply") {}
+
+}
