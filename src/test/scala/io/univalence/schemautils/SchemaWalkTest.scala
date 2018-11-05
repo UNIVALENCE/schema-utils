908b57dff946fd9ac5f68aaa27ea196cb6f0c4c7
François Sarradin <fsarradin@gmail.com>
2018-11-05 17:07:08 +0100

    add test on validateSchema

--- src/test/scala/io/univalence/schemautils/SchemaWalkTest.scala
+++ src/test/scala/io/univalence/schemautils/SchemaWalkTest.scala
@@ -1,9 +1,13 @@
 package io.univalence.schemautils
 
+import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.types._
 import org.scalatest.{FunSuiteLike, Matchers}
 
 class SchemaWalkTest extends FunSuiteLike with Matchers {
+
+  import TestSparkSession.ss
+  import ss.implicits._
 
   test("should have same data type when same type") {
     SchemaWalk.sameDatatype(IntegerType, IntegerType) should be(true)
@@ -30,6 +34,34 @@ class SchemaWalkTest extends FunSuiteLike with Matchers {
   test("should not have same data type for structure with different nested types and same field name") {
     SchemaWalk.sameDatatype(StructType(StructField("a", IntegerType) :: Nil),
                             StructType(StructField("a", BooleanType) :: Nil)) should be(false)
+  }
+
+  test("should validate dataframe when its schema matches the contract") {
+    val df: DataFrame =
+      Seq(
+        ("1", Array(1, 2))
+      ).toDF("id", "values")
+
+    (SchemaWalk.validateSchema(df,
+                               StructType(
+                                 StructField("id", StringType)
+                                   :: StructField("values", ArrayType(IntegerType))
+                                   :: Nil))
+      should be(Some(df)))
+  }
+
+  test("should not validate dataframe when its schema does not match the contract") {
+    val df: DataFrame =
+      Seq(
+        ("1", Array(1, 2))
+      ).toDF("id", "values")
+
+    (SchemaWalk.validateSchema(df,
+                               StructType(
+                                 StructField("id", StringType)
+                                   :: StructField("values", ArrayType(BooleanType))
+                                   :: Nil))
+      should be(None))
   }
 
 }
